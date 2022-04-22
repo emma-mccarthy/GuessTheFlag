68d474d66519b0e2982368bea492a4d7fe72e11f
emma-mccarthy <104154017+emma-mccarthy@users.noreply.github.com>
2022-04-22 15:42:49 +0100

    Initial commit

---
+++ GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Emma McCarthy on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
